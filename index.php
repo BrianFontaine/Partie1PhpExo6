c4e6e385818d9efe4efc10672c07a457392b20a8
Brian Fontaine <brian02440@live.fr>
2020-06-05 12:23:10 +0200

    exo6

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    include'../header.php';
+    $name = 'Brian';
+?>
+<body>
+    <h1>Exercices 6</h1>
+    <p><?="bonjour je m'apelle ".$name;?><p>
+</body>
+<?php
+    include'../footer.php';
+?>
